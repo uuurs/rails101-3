41b6df293beb2987b0c96886d97fd03e766792ba
uuurs <zhongrenju2016@gmail.com>
2017-05-28 13:15:56 +0800

    implement edit in my_post table

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, :onlu => [:new, :create]
+  before_action :authenticate_user!, :onlu => [:new, :create, :edit, :update]
+  before_action :find_group_and_post, :only => [:edit, :update]
 
   def new
     @group = Group.find(params[:group_id])
@@ -19,11 +20,27 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to account_posts_path(@group), notice: "Update succsess"
+    else
+      render :edit
+    end
+  end
+
 
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
-  
+
+  def find_group_and_post
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
 end
